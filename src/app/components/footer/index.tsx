134720b25d9404bd88f0a86ccc6835ba556de86d
dietcokechan <dietcokechan@gmail.com>
2025-02-18 16:27:37 +0200

    footer with valid links

---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+import Link from 'next/link'
+import { Github as GitHubIcon, Linkedin as LinkedinIcon, Mail as MailIcon } from 'lucide-react';
+
+export default function Footer() {
+    return (
+        <>
+            <div className="w-full h-16 bg-[#1E1E1E] bottom-0 border-t z-10">
+                <div className="mx-4 px-0 h-full">
+                    <div className="flex justify-between items-center h-full divide-x text-gray-300">
+                        <div className="flex justify-between items-center divide-x">
+                            <p className="px-6 py-4 font-medium text-white">
+                                links
+                            </p>
+                            <ul className="flex text-white justify-center items-center divide-x">
+                                <li className="px-6 py-4 cursor-pointer">
+                                    <Link href="https://linkedin.com/in/habiba-sameh" target="_blank">
+                                        <LinkedinIcon />
+                                    </Link>
+                                </li>
+                                <li className="px-6 py-4 cursor-pointer">
+                                    <Link href="mailto:habibasamehmosa@gmail.com" target="_blank">
+                                        <MailIcon />
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <Link href="https://github.com/dietcokechan" target="_blank" className="flex gap-x-2 text-white pl-6 py-4 cursor-pointer">
+                            <p>@dietcokechan</p>
+                            <GitHubIcon />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-
+      <div className="flex-grow flex items-center justify-center bg-[#272727] text-white">
+          <h1 className="text-6xl">Main Content</h1>
+      </div>
     </>
   );
 }
