edeaea93bfdfb5ec3399a79fe68033910adf0334
dietcokechan <dietcokechan@gmail.com>
2025-02-19 13:34:15 +0200

    navbar values fixes

--- src/app/components/navigation/explorer/index.tsx
+++ src/app/components/navigation/explorer/index.tsx
@@ -2,7 +2,7 @@
 
 export default function Explorer() {
     return (
-        <aside className="w-14 h-[calc(100vh-4rem)] sticky -top-1/4 flex flex-col items-center space-y-6 py-4 border-r border-white bg-[#1E1E1E]">
+        <aside className="w-14 h-[calc(100vh-4rem)] sticky top-12 flex flex-col items-center space-y-6 py-4 border-r border-white bg-[#1E1E1E]">
             <div className="hover:text-white cursor-pointer">
                 <FolderIcon className="size-5" />
             </div>
